e4b750a436bf95e54ff337954cc0fabcd93a5c83
NSQA <nsqa99@github.com>
2021-07-21 09:25:21 +0700

    add i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get "/about", to: "static_pages#about"
-  get "/register", to: "users#new"
-
-  resources :users
-
-  root "static_pages#home"
+  scope "(:locale)", locale: /en|vi/ do
+    get "/about", to: "static_pages#about"
+    get "/register", to: "users#new"
+  
+    resources :users
+  
+    root "static_pages#home"
+  end
 end
