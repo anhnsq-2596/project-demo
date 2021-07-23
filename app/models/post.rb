034c7ef179511f426cb76dff822cf1e847d0debd
NSQA <nsqa99@github.com>
2021-07-23 14:55:37 +0700

    fix: conflict;

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,8 @@ class Post
   has_and_belongs_to_many :tags
 
   validates :content, presence: true, length: { maximum: 250 }
+
+  def local_created_at
+    created_at.localtime.strftime("%H:%M:%S - %d/%m/%Y")
+  end
 end
