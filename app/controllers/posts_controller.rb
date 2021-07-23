ab975456f24538ed76745a1a26a1b1ad1b12db62
NSQA <nsqa99@github.com>
2021-07-23 14:56:38 +0700

    fix: conflict

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,6 @@ class PostsController < ApplicationController
   before_action :validate_user, only: [:edit, :update, :destroy]
   
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
