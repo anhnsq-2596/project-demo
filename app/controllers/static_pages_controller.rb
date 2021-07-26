66344176903fc36aee05277d34d155d5257a792c
NSQA <nsqa99@github.com>
2021-07-26 13:30:18 +0700

    style: single to double quotes & chore: change variable name static_page controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     if params[:filter].present?
-      tag_id = Tag.where(label: params[:filter]).pluck(:id)
+      tag_ids = Tag.where(label: params[:filter]).pluck(:id)
       @posts = Post.where(:tag_ids.in => tag_id).page(params[:page])
     else
       @posts = Post.search(params[:search]).desc_order_by_created_at.page(
