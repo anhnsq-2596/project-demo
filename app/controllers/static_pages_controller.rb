cf4405409aef50ba36b063d83449cad75028e698
NSQA <nsqa99@github.com>
2021-07-26 17:04:10 +0700

    style: single to double quote & restyle colors

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,8 @@ class StaticPagesController < ApplicationController
       tag_ids = Tag.where(label: params[:filter]).pluck(:id)
       @posts = Post.where(:tag_ids.in => tag_ids).page(params[:page])
     else
-      @posts = Post.search(params[:search]).desc_order_by_created_at.page(
-        params[:page])
+      @posts = Post.search(params[:search]).desc_order_by_created_at
+        .page(params[:page])
     end
     @tags = Tag.all
   end
