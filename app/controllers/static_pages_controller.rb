1fbf9d15eb7c6da32f39783c3e27f777db6851a9
NSQA <nsqa99@github.com>
2021-07-26 22:10:29 +0700

    fix: add ordering to post when filter

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     if params[:filter].present?
       tag_ids = Tag.where(label: params[:filter]).pluck(:id)
-      @posts = Post.where(:tag_ids.in => tag_ids).page(params[:page])
+      @posts = Post.where(:tag_ids.in => tag_ids).desc_order_by_created_at
+        .page(params[:page])
     else
       @posts = Post.search(params[:search]).desc_order_by_created_at
         .page(params[:page])
