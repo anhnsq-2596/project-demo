fa78d612ec0afbb5ce9faabfd0c208285d4a47d4
NSQA <nsqa99@github.com>
2021-07-26 13:31:40 +0700

    fix: variable match static_page controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if params[:filter].present?
       tag_ids = Tag.where(label: params[:filter]).pluck(:id)
-      @posts = Post.where(:tag_ids.in => tag_id).page(params[:page])
+      @posts = Post.where(:tag_ids.in => tag_ids).page(params[:page])
     else
       @posts = Post.search(params[:search]).desc_order_by_created_at.page(
         params[:page])
